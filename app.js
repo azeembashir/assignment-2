e6c3b6d11491419d078addae757ef9fdd42e51db
Azeem Bashir <azeembashir.33@gmail.com>
2024-08-29 22:32:16 +0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,49 @@
+
+//Chapter#3
+//question1
+//var age = "I am 15 years old";
+//alert(age)
+
+//question2
+//var n = 3;
+//var visit = "you have visited this sites " ;
+//alert(visit + n +" times" )
+
+//question3
+//var birthYear = 24;
+//var description = "My Birth Year is " + birthYear + "</br>";
+//document.write(description);
+//document.write("\data type is " + typeof birthYear);
+
+//question4
+// var visitor_name = "<b>John Doe</b>";
+// var product_tittle = "<b>XYZ clothing store</b>";
+// var quantity = "<b>5 T-shirts</b>";
+// document.write(visitor_name + " ordered " + quantity + " on " + product_tittle );
+
+//chapter#4
+
+//question1
+//let x= 20, y= 15, z= 25;
+
+//question2
+//legal
+// firstName
+// _age
+// totalAmount
+// $price
+// isAvailable
+
+//illegal
+// 2ndPlace 
+// var 
+// user-name 
+// @email 
+// my variable 
+
+//question3
+document.write("<h1>Rules for naming JS variables</h1>");
+document.write("<p>Variable names can only contain, numbers, $ and _  For example $my_1stVariable</p>")
+document.write("<p>Variables must begin with a letters, $ or _  For example $name, _name or name</p>")
+document.write("<p>Variable names are case sensitive.</p>")
+document.write("<p>Variable names should not be JS keywords.</p>")
